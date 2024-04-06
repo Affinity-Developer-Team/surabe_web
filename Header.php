b5a3d9161be524087c46ded4e35ce2a61b2eba4f
Thinuka20 <140114175+Thinuka20@users.noreply.github.com>
2024-04-06 13:30:58 +0530

    full code

--- Header.php
+++ Header.php
@@ -7,80 +7,240 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
     <title>Document</title>
-    <style>
-
-        header {
-            background: transparent;
-            padding: 15px;
-            position: fixed;
-            width: 100%;
-            top: 0;
-            /* z-index: 1000; */
-            transition: background 0.3s ease;
-            overflow: visible;
-        }
-
-        header.scrolled {
-            background: transparent;
-            backdrop-filter: blur(20px);
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            position: fixed;
-        }
-
-        header a {
-            text-decoration: none;
-            font-size: 18px;
-            transition: color 0.3s ease; /* Smooth transition for color change */
-        }
-
-        header a.header-topics {
-            margin: 0 15px;
-            color: black; /* Initial color for header topics */
-        }
-    </style>
 </head>
 
-<body>
+<body style="overflow-x: hidden; background-color: #c9f8c8">
     <header class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a href="#" class="header-topics text-black">Brand Name/Logo</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a href="#" class="nav-link header-topics">Home</a>
+                        <a href="#" class="nav-link header-topics active" aria-current="page">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link header-topics">About</a>
+                        <a href="#" class="nav-link header-topics active">About</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link header-topics">Packages</a>
+                        <a href="#" class="nav-link header-topics active">Packages</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link header-topics">Contact</a>
+                        <a href="#" class="nav-link header-topics active">Contact</a>
                     </li>
                 </ul>
             </div>
         </div>
     </header>
 
+    <div class="row">
+
+        <!-- Left Column -->
+
+        <div class="col-lg-7 col-md-12 d-flex align-items-center" style="background-color: #C9F8C8; height: 600px;">
+            <div class="container">
+                <div class="col-lg-10 offset-lg-2">
+                    <div class="row">
+                        <div class="">
+                            <p class="home-header">SURABE</p>
+                        </div>
+                        <div class="">
+                            <p class="home-subheader">Clinic</p>
+                        </div>
+                        <div class="">
+                            <p class="home-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+                                consequuntur sunt eius minus amet placeat natus obcaecati exercitationem, odit dolorem
+                                ipsam
+                                labore. Ratione est laborum cumque facere quae provident officia.</p>
+                        </div>
+                        <div class="">
+                            <div class="row">
+                                <div class="col-12 d-flex justify-content-start mb-2">
+                                    <div class="button-container me-2">
+                                        <a href="#" class="animated-button">Register</a>
+                                    </div>
+                                    <div class="button-container">
+                                        <a href="#" class="animated-button2">Booking</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Right Column -->
+        <div class="col-lg-5 d-none d-lg-block" style="background-color: #2F682E;">
+
+        </div>
+    </div>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- about us -->
+
+    <div class="container">
+        <div class="content row d-flex align-content-center" style="width: 100%; height: 60vh;">
+            <h1 class="text-center ha">About Us</h1>
+            <p class="text-center myp mt-5">
+                Lorem ipsum is a placeholder text used in publishing and graphic
+                design. It's often used to show the visual form of a document or a
+                typeface without using meaningful content. Lorem ipsum can also be
+                used as a placeholder before the final copy is available.Lorem ipsum
+                is a placeholder text used in publishing and graphic design. It's
+                often used to show the visual form of a document or a typeface
+                without using meaningful content. Lorem ipsum can also be used as a
+                placeholder before the final copy is available. Lorem ipsum is a
+                placeholder text used in publishing and graphic design. It's often
+                used to show the visual form of a document or a typeface without
+                using meaningful content. Lorem ipsum can also be used as a
+                placeholder before the final copy is available.
+            </p>
+            <div class="row">
+                <div class="col-6 d-flex justify-content-end">
+                    <button class="bn bn1"><i class="fa-brands fa-facebook"></i> FaceBook</button>
+
+                </div>
+                <div class="col-6">
+                    <button class="bn bn2"><i class="fa-brands fa-whatsapp"></i><span class="ms-1">Whatsapp</span> </button>
+
+                </div>
+
+
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class="bne mt-4 me-3">Explore More</button>
+            </div>
+        </div>
+    </div>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- services -->
+    <div class="container">
+        <div class="wrapper">
+            <h1>Our Services</h1>
+
+            <div class="content-box">
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+            </div>
+
+            <div>
+                <button class="More-Services btn1">More Services</button>
+            </div>
+        </div>
+    </div>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- contact info -->
+    <?php include "contact.php"; ?>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- footer -->
+    <div class="container-fluid" style="background-color: #2f682e; margin-bottom: 0px;">
+        <div class="container">
+            <div class="content row d-flex align-content-center" style="height: 60vh; width: 100%">
+                <h2 class="text-light text-center fthadder">SURABE CLINIC</h2>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 650px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+
+                <p class="text-center" style="color: rgb(255, 255, 255);">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+                    deserunt minus numquam labore corrupti hic rem rerum eius libero
+                    nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Quam culpa libero molestiae, voluptatibus quasi harum reiciendis
+                    itaque omnis expedita reprehenderit!
+                </p>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 300px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+                <div class="d-flex justify-content-center">
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new" />
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script>
-        window.addEventListener("scroll", function () {
+        window.addEventListener("scroll", function() {
             var header = document.querySelector("header");
             var topics = document.querySelectorAll(".header-topics");
 
             if (window.scrollY > 0) {
                 header.classList.add("scrolled");
-                topics.forEach(function (topic) {
+                topics.forEach(function(topic) {
                     topic.style.color = "white"; // Change to your desired scrolled color
                 });
             } else {
                 header.classList.remove("scrolled");
-                topics.forEach(function (topic) {
+                topics.forEach(function(topic) {
                     topic.style.color = "black"; // Change to your desired initial color
                 });
             }
--- index.php
+++ index.php
@@ -2,33 +2,300 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css" />
     <title>Document</title>
+    <style>
+        #barcontainer{
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 255, 255, 0.9);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .bars {
+            width: 50.5px;
+            height: 50.5px;
+            --c: conic-gradient(from -90deg, #61e85f 90deg, #0000 0);
+            background: var(--c), var(--c);
+            background-size: 20.2px 20.2px;
+            animation: bars-dxe116md 1.2000000000000002s infinite;
+        }
+
+        @keyframes bars-dxe116md {
+
+            0%,
+            20% {
+                background-position: 0 0, 0 10.1px;
+            }
+
+            33% {
+                background-position: 0 0, 10.1px 10.1px;
+            }
+
+            66% {
+                background-position: 0 10.1px, 10.1px 0;
+            }
+
+            80%,
+            100% {
+                background-position: 0 10.1px, 0 0;
+            }
+        }
+    </style>
 </head>
 
-<body>
+<body style="overflow-x: hidden;">
+    <div id="barcontainer">
+        <div class="bars"></div>
+    </div>
+    <header class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a href="#" class="header-topics text-black">Surabe Clinic</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link header-topics active" aria-current="page">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link header-topics active">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link header-topics active">Packages</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link header-topics active">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
 
-<?php include "Header.php"; ?>
+    <div class="row">
 
-<?php include "Home.php"; ?>
+        <!-- Left Column -->
 
-<br> <br> <br><br> <br> <br>
+        <div class="col-lg-7 col-md-12 d-flex align-items-center" style="background-color: #C9F8C8; height: 600px;">
+            <div class="container">
+                <div class="col-lg-10 offset-lg-2">
+                    <div class="row">
+                        <div class="">
+                            <p class="home-header">SURABE</p>
+                        </div>
+                        <div class="">
+                            <p class="home-subheader">Clinic</p>
+                        </div>
+                        <div class="">
+                            <p class="home-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo,
+                                consequuntur sunt eius minus amet placeat natus obcaecati exercitationem, odit dolorem
+                                ipsam
+                                labore. Ratione est laborum cumque facere quae provident officia.</p>
+                        </div>
+                        <div class="">
+                            <div class="row">
+                                <div class="col-12 d-flex justify-content-start mb-2">
+                                    <div class="button-container me-2">
+                                        <a href="#" class="animated-button">Register</a>
+                                    </div>
+                                    <div class="button-container">
+                                        <a href="#" class="animated-button2">Booking</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-<?php include "about_us.php"; ?>
+        <!-- Right Column -->
+        <div class="col-lg-5 d-none d-lg-block" style="background-color: #2F682E;">
 
-<br> <br> <br><br> <br> <br>
+        </div>
+    </div>
 
-<?php include "services.php"; ?>
+    <br> <br> <br> <br> <br>
 
-<br> <br> <br><br> <br> <br>
+    <!-- about us -->
 
-<?php include "contact.php"; ?>
+    <div class="container">
+        <div class="content row d-flex align-content-center" style="width: 100%; height: 60vh;">
+            <h1 class="text-center ha">About Us</h1>
+            <p class="text-center myp mt-5">
+                Lorem ipsum is a placeholder text used in publishing and graphic
+                design. It's often used to show the visual form of a document or a
+                typeface without using meaningful content. Lorem ipsum can also be
+                used as a placeholder before the final copy is available.Lorem ipsum
+                is a placeholder text used in publishing and graphic design. It's
+                often used to show the visual form of a document or a typeface
+                without using meaningful content. Lorem ipsum can also be used as a
+                placeholder before the final copy is available. Lorem ipsum is a
+                placeholder text used in publishing and graphic design. It's often
+                used to show the visual form of a document or a typeface without
+                using meaningful content. Lorem ipsum can also be used as a
+                placeholder before the final copy is available.
+            </p>
+            <div class="row">
+                <div class="col-6 d-flex justify-content-end">
+                    <button class="bn bn1"><i class="fa-brands fa-facebook"></i> FaceBook</button>
 
-<br> <br> <br><br> <br> <br>
+                </div>
+                <div class="col-6">
+                    <button class="bn bn2"><i class="fa-brands fa-whatsapp"></i><span class="ms-1">Whatsapp</span> </button>
 
-<?php include "footer.php"; ?>
+                </div>
 
+
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class="bne mt-4 me-3">Explore More</button>
+            </div>
+        </div>
+    </div>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- services -->
+    <div class="container">
+        <div class="wrapper">
+            <h1>Our Services</h1>
+
+            <div class="content-box">
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+
+                <div class="card-box">
+                    <h2>Services Name</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+                        laborum hic uod reiciendis similique. Explicabo fugit ullam quo
+                        saepe!
+                    </p>
+                </div>
+            </div>
+
+            <div>
+                <button class="More-Services btn1">More Services</button>
+            </div>
+        </div>
+    </div>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- contact info -->
+    <?php include "contact.php"; ?>
+
+    <br> <br> <br> <br> <br>
+
+    <!-- footer -->
+    <div class="container-fluid" style="background-color: #2f682e; margin-bottom: 0px;">
+        <div class="container">
+            <div class="content row d-flex align-content-center" style="height: 60vh; width: 100%">
+                <h2 class="text-light text-center fthadder">SURABE CLINIC</h2>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 650px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+
+                <p class="text-center" style="color: rgb(255, 255, 255);">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+                    deserunt minus numquam labore corrupti hic rem rerum eius libero
+                    nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Quam culpa libero molestiae, voluptatibus quasi harum reiciendis
+                    itaque omnis expedita reprehenderit!
+                </p>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 300px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+                <div class="d-flex justify-content-center">
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new" />
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <script>
+        window.addEventListener('load', function() {
+            var loadingAnimation = document.getElementById('barcontainer');
+            loadingAnimation.style.display = 'none';
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script>
+        window.addEventListener("scroll", function() {
+            var header = document.querySelector("header");
+            var topics = document.querySelectorAll(".header-topics");
+
+            if (window.scrollY > 0) {
+                header.classList.add("scrolled");
+                topics.forEach(function(topic) {
+                    topic.style.color = "white"; // Change to your desired scrolled color
+                });
+            } else {
+                header.classList.remove("scrolled");
+                topics.forEach(function(topic) {
+                    topic.style.color = "black"; // Change to your desired initial color
+                });
+            }
+        });
+    </script>
 </body>
 
 </html>
