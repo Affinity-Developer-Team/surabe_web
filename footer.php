9bc6a62ac958ac8b873d8169bdc9bee19fe9cb0e
Lochana Pahansilu <lochanamax38@gmail.com>
2024-04-12 14:08:14 +0530

    header eke smooth eka haduwa booking eke footer eka haduwa loading ekak haduwa

--- footer.php
+++ footer.php
@@ -5,12 +5,20 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/bootstrap.css">
+    
+
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Istok+Web:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        
+        .fthadder {
+            font-family: 'Istok Web', sans-serif;
+            letter-spacing: 0.5px;
+            font-weight: bold;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -22,7 +30,7 @@
                     <hr class="mt-4 mb-4" style="width: 650px; height: 2px; background-color: rgb(255, 255, 255);">
 
                 </div>
-                
+
                 <p class="text-center" style="color: rgb(255, 255, 255);">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
                     deserunt minus numquam labore corrupti hic rem rerum eius libero
@@ -35,12 +43,12 @@
 
                 </div>
                 <div class="d-flex justify-content-center">
-                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new"/>
-                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1"/>
-                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new"/>
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1" />
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new" />
 
                 </div>
-                
+
             </div>
         </div>
     </div>
