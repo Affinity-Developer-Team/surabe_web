5c6cdda928ef8b31b40e13f6f7e77412985a42bd
Thinuka20 <140114175+Thinuka20@users.noreply.github.com>
2024-04-13 20:41:07 +0530

    registration edited

--- registrations.php
+++ registrations.php
@@ -99,17 +99,17 @@
             <div class="row">
               <div class="col-12">
                 <label class="form-label fw-bold mt-3">Email:</label>
-                <input type="text" class="form-control" id="email" placeholder="First Name">
+                <input type="text" class="form-control" id="email" placeholder="email">
               </div>
             </div>
             <div class="row">
               <div class="col-lg-6">
                 <label class="form-label fw-bold mt-3">Mobile Number:</label>
-                <input type="text" class="form-control" id="mobile" placeholder="First Name">
+                <input type="text" class="form-control" id="mobile" placeholder="mobile">
               </div>
               <div class="col-lg-6">
                 <label class="form-label fw-bold mt-3">Whatsapp Number:</label>
-                <input type="text" class="form-control" id="whatsapp" placeholder="Last Name">
+                <input type="text" class="form-control" id="whatsapp" placeholder="Whatsapp">
               </div>
             </div>
             <div class="row">
@@ -123,31 +123,63 @@
               </div>
               <div class="col-lg-6">
                 <label class="form-label fw-bold mt-3">Date Of Birth:</label>
-                <input type="date" class="form-control" id="dob" placeholder="Last Name">
+                <input type="date" class="form-control" id="dob">
               </div>
             </div>
             <div class="row">
               <div class="col-lg-6">
                 <label class="form-label fw-bold mt-3">Country:</label>
-                <input type="text" class="form-control" id="country" placeholder="First Name">
+                <input type="text" class="form-control" id="country" placeholder="Country">
               </div>
               <div class="col-lg-6">
                 <label class="form-label fw-bold mt-3">City:</label>
-                <input type="text" class="form-control" id="city" placeholder="Last Name">
+                <input type="text" class="form-control" id="city" placeholder="City">
               </div>
             </div>
             <div class="row">
               <div class="col-12">
                 <label class="form-label fw-bold mt-3">Address:</label>
-                <input type="text" class="form-control" id="address" placeholder="First Name">
+                <input type="text" class="form-control" id="address" placeholder="Address">
               </div>
+            </div>
+            <div class=" d-flex justify-content-center">
+              <button class="btn btn-success mt-3">Register Now</button>
             </div>
           </div>
         </section>
       </div>
     </div>
   </div>
-  <?php include "footer.php"; ?>
+  <div class="container-fluid" style="background-color: #2f682e;">
+        <div class="container">
+            <div class="content row d-flex align-content-center" style="height: 60vh; width: 100%">
+                <h2 class="text-light text-center fthadder">SURABE CLINIC</h2>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 650px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+                
+                <p class="text-center" style="color: rgb(255, 255, 255);">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam
+                    deserunt minus numquam labore corrupti hic rem rerum eius libero
+                    nostrum. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Quam culpa libero molestiae, voluptatibus quasi harum reiciendis
+                    itaque omnis expedita reprehenderit!
+                </p>
+                <div class="d-flex justify-content-center">
+                    <hr class="mt-4 mb-4" style="width: 300px; height: 2px; background-color: rgb(255, 255, 255);">
+
+                </div>
+                <div class="d-flex justify-content-center">
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/facebook-new.png" alt="facebook-new"/>
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/color/48/whatsapp--v1.png" alt="whatsapp--v1"/>
+                    <img class="me-4" width="48" height="48" src="https://img.icons8.com/fluency/48/instagram-new.png" alt="instagram-new"/>
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
   <script>
     window.addEventListener('load', function() {
       var loadingAnimation = document.getElementById('barcontainer');
